ec4cd91bca8305a43a78aad222641661dbdfdbe2
Vivekananda Bediya <vivekanandbediya006@gmail.com>
2025-12-19 00:36:21 +0530

    add the backgroud image and sun logo according to the weather condition

--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
+// calling/set default value from api 
+function getWeather(cityName) {
+    const city_name = "New Delhi";
+    const state_code = "DL";
+    const country_code = "IN";
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${state_code},${country_code}&limit=1&appid=86172535ea5859db60396039ab393716`)
+    .then(response => response.json())
+    .then(result => {
+        const latitude = result[0].lat;
+        const longitude = result[0].lon;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=86172535ea5859db60396039ab393716`)
+        .then(response => response.json())
+        .then(result2 => {
+            const temp = result2.main.temp;
+            const weather = result2.weather[0].description;
+            const feels_like = result2.main.feels_like;
+
+            const wind_speed = result2.wind.speed;
+            const humidity = result2.main.humidity;
+            const air_cond = result2.main.pressure;
+            const name = result2.name;
+            const cntry = result2.sys.country;
+
+            const Celsius=(temp - 273.15)
+            console.log("Temperature : ",Celsius.toFixed(2)+" °C")
+
+            // const Celsius=(temp - 273.15) // Celsius=Kelvin−273.15
+            document.getElementById("temp").innerHTML = Celsius.toFixed(2)+" °C";
+            document.getElementById("weather_desc").innerHTML = weather;
+            const feels_like_cel = (feels_like - 273.15)
+            document.getElementById("feels_like").innerHTML = ("Feels like "+feels_like_cel.toFixed(2)+" °C")
+            document.getElementById('wind_speed').innerHTML = (Math.round(wind_speed*3.6))+" km/h";  // km/h=m/s×3.6
+            document.getElementById("humidity").innerHTML = (humidity+" %")
+            document.getElementById("pressure").innerHTML = air_cond+" hPa";
+            document.getElementById("location").innerHTML = `${name} ,${cntry}`;
+
+            if(weather === "clear sky"){  //Shillong
+                document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-cloud-sun" style="color: white"></i>';
+            }else if(weather == "fog" || "mist"){
+                document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-cloud" style="color: white"></i>'
+            }else if(weather == "rainy"){
+                document.getElementById("sun_logo").innerHTML ='<i class="fa-solid fa-cloud-sun-rain" style="color:white;"></i>';
+            }else{
+                document.getElementById("sun_logo").innerHTML = '🌤';
+            }
+        })
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+}
 
 
+// get the seaching data
 async function fetchData(){
     const city = document.getElementById("city").value;
     const state = document.getElementById("state").value;
@@ -43,14 +96,14 @@ async function fetchData(){
     
     
 
-    console.log("temp : ",temp)
-    console.log("weather : ",weather)
-    console.log("feels_like : ",feels_like)
-    console.log("wind_speed : ",wind_speed)
-    console.log("humidity : ",humidity)
-    console.log("Air condition : ",air_cond)
-    console.log("city : ",name)
-    console.log("counrty : ",cntry)
+    // console.log("temp : ",temp)
+    // console.log("weather : ",weather)
+    // console.log("feels_like : ",feels_like)
+    // console.log("wind_speed : ",wind_speed)
+    // console.log("humidity : ",humidity)
+    // console.log("Air condition : ",air_cond)
+    // console.log("city : ",name)
+    // console.log("counrty : ",cntry)
 
 
     const Celsius=(temp - 273.15)
@@ -67,7 +120,21 @@ async function fetchData(){
     document.getElementById("pressure").innerHTML = air_cond+" hPa";
     document.getElementById("location").innerHTML = `${name} ,${cntry}`;
 
+    if(weather === "clear sky"){                         //Shillong
+        document.body.classList.add("bg-clear_sky") 
+        document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-cloud-sun" style="color: white"></i>';
 
+    }else if(weather === "fog" || "mist"){                //RANCHI, GURUGRAM
+        document.body.classList.add("bg-FOG")
+        document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-cloud" style="color: white"></i>'
+
+    }else if(weather === "rainy"){
+        document.body.classList.add("bg-rainy_sky")
+        document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-cloud-sun-rain" style="color: white;"></i>';
+
+    }else{
+        document.getElementById("sun_logo").innerHTML = '<i class="fa-solid fa-sun" style="color: white"></i>'
+    }
 
 }
 
